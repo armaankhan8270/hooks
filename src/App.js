301efbc494268ab472cc80766d07c064d573bebb
armaan <karmankkhan@gmail.com>
2022-10-02 12:21:25 +0530

    start

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App ">
+      <p className=" sm:text-sm md:text-2xl lg:text-5xl ">armaan</p>
+      <div className="shadow-xl shadow-slate-800 m-4 h-40  sm:w-1/3 md:w-1/2 lg:w-full ">
+        {" "}
+      </div>
+    </div>
+  );
+}
+
+export default App;
